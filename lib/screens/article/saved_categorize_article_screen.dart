854e1adcbbeecca5aa4094e332aeb0837f7f0515
jpeeicruz <jp.delacruz@kabootekph.com>
2022-05-27 13:17:45 +0800

    RTE-233 able to select article in CategorizeArticleScreen

--- lib/screens/article/saved_categorize_article_screen.dart
+++ lib/screens/article/saved_categorize_article_screen.dart
@@ -1,8 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:rte_app/blocs/articles/articles_bloc.dart';
+import 'package:rte_app/blocs/articles/articles_event.dart';
 import 'package:rte_app/common/constants.dart';
 import 'package:rte_app/common/size_config.dart';
+import 'package:rte_app/common/string_routes.dart';
 import 'package:rte_app/models/article.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rte_app/models/screen_arguments.dart';
+import 'package:rte_app/screens/article/show_article_screen.dart';
 
 class SavedCategorizeArticleScreen extends StatefulWidget {
   final String pageName;
@@ -37,30 +43,40 @@ class _SavedCategorizeArticleScreenState extends State<SavedCategorizeArticleScr
         ),
         itemCount: widget.categorizedArticles.length,
         itemBuilder: (_, int index) {
-          return SizedBox(
-            child: Card(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CachedNetworkImage(
-                    imageUrl: '${dev_endpoint}/articles/articles-default.jpg',
-                    fit: BoxFit.contain,
-                  ),
-                  ListTile(
-                    title: Text(
-                      widget.categorizedArticles[index].articleTitle.toString(),
-                      style: Theme.of(context).textTheme.subtitle2,
+          return GestureDetector(
+            onTap: () {
+              context.read<ArticlesBloc>().add(CurrentReadArticle(article: widget.categorizedArticles[index]));
+              context.read<ArticlesBloc>().add(ArticleGetContentEvent(id: widget.categorizedArticles[index].id!));
+              context.read<ArticlesBloc>().add(GetViolations());
+              Navigator.pushNamed(context, read_article,
+                  arguments: ScreenArguments(
+                      article: widget.categorizedArticles[index], isViewedSavedArticle: true));
+            },
+            child: SizedBox(
+              child: Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CachedNetworkImage(
+                      imageUrl: '${dev_endpoint}/articles/articles-default.jpg',
+                      fit: BoxFit.contain,
                     ),
-                    subtitle: Text(
-                      widget.categorizedArticles[index].author.toString(),
-                      style: Theme.of(context).textTheme.subtitle2!.copyWith(
-                        color: COLOR_DARK_GRAY,
-                        fontSize: SizeConfig.blockSizeVertical! * 1.7,
+                    ListTile(
+                      title: Text(
+                        widget.categorizedArticles[index].articleTitle.toString(),
+                        style: Theme.of(context).textTheme.subtitle2,
                       ),
-                    ),
-                  )
-                ],
+                      subtitle: Text(
+                        widget.categorizedArticles[index].author.toString(),
+                        style: Theme.of(context).textTheme.subtitle2!.copyWith(
+                          color: COLOR_DARK_GRAY,
+                          fontSize: SizeConfig.blockSizeVertical! * 1.7,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           );
